6f378f5e8af6b60d9f6d9feb8d9e6fe7dc06d957
Abinoam P. Marques Jr <abinoam@gmail.com>
2017-11-06 20:22:30 -0300

    Add a failing spec for #19

--- spec/integration/inflector_spec.rb
+++ spec/integration/inflector_spec.rb
@@ -4,4 +4,17 @@ describe Flexus, 'underscore' do
   specify 'allows to create snake_case from CamelCase' do
     expect(Flexus.underscore('CamelCase')).to eql('camel_case')
   end
+
+  describe 'allows to use per instance inflections' do
+    let(:flexus_instance) { Flexus.new }
+
+    before do
+      flexus_instance.inflections do |inflect|
+        inflect.plural(/\z/, 'zishes')
+      end
+    end
+
+    it { expect(Flexus.pluralize("dog")).to          eq "dogs" }
+    it { expect(flexus_instance.pluralize("dog")).to eq "dogzishes" }
+  end
 end
